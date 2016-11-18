be0bacd9b9db0ab7c794eda3641874d94d9bbba9
Nathan Parrish <nathanielparrish@gmail.com>
2016-11-18 11:32:13 -0800

    Add camera to the scene

--- src/app/reducers/scene.ts
+++ src/app/reducers/scene.ts
@@ -1,6 +1,12 @@
 import { Reducer } from 'redux';
 
-export const scene: Reducer<{}> = (state = [{ type: 'sphere', position: { x: 0, y: 0, z: 0 }, color: { r: 255, g: 0, b: 0, a: 255 } }], action) => {
+export const scene: Reducer<{}> = (state = [
+    {
+        type: 'sphere', position: { x: 0, y: 0, z: 0 }, color: { r: 255, g: 0, b: 0, a: 255 }
+    },
+    {
+        type: 'camera', position: { x: 0, y: 0, z: -40 }, direction: { x: 0, y: 0, z: 1 }, up: { x: 0, y: 1, z: 0 }
+    }], action) => {
     switch (action.type) {
         default:
             return state;
